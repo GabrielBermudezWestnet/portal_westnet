7c69b6488ce3cfaa8e37e3cffecfb0675adac268
Gabriel <gbermudez.westnet@gmail.com>
2021-07-07 20:36:23 +0000

    Subo primera version del portal

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '192.168.2.115',
+      ref  : 'origin/master',
+      repo : '',
+      path : '',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
---
+++ routes/portal.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/success', function(req, res, next) {
+  res.render('success');
+});
+
+router.get('/bill-payed', function(req, res, next) {
+    res.render('bill-payed');
+  });
+
+router.get('/error-intention-payment', function(req, res, next) {
+    res.render('error-intention-payment');
+});
+
+router.get('/system-disabled', function(req, res, next) {
+    res.render('system-disabled');
+});
+
+router.get('/error-bill-closed', function(req, res, next) {
+  res.render('error-bill-closed');
+});
+error-bill-closed
+module.exports = router;
