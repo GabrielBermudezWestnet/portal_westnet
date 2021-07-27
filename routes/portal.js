1163ed5731c2ce1fc1a302250f17ad251efbc640
emilasheras <emperorbfd@gmail.com>
2021-07-27 14:59:18 -0300

    se agregan los mensajes a portal.js en forma de un objeto js

--- routes/portal.js
+++ routes/portal.js
@@ -1,30 +1,48 @@
 var express = require('express');
 var router = express.Router();
+companyName = "Westnet S.A";
+msgObject = {
+  success : "Su pago ha sido registrado\nMuchas gracias por usar nuestros servicios de pago",
+  billPayed : "Su factura se encuentra pagada.\nMuchas gracias por usar nuestros servicios de pago",
+  errorIntentionPayment : "Su link de pago no pudo ser generado.\nAguarde unos minutos e intente nuevamente!\nMuchas gracias por usar nuestros servicios de pago",
+  systemDisabled : "El sistema se encuentra inhabilitado, por favor, aguarde unos instantes.\nMuchas gracias por usar nuestros servicios de pago",
+  errorBillDraft : "Esta factura no es valida para ser pagada.\nMuchas gracias por usar nuestros servicios de pago",
+};
 
 /* GET home page. */
 router.get('/success', function(req, res, next) {
-  companyName = "Westnet S.A";
-  msg = "Su pago ha sido registrado\nMuchas gracias por usar nuestros servicios de pago";
   res.render('success', {
-    'redirectMessage' : msg,
+    'redirectMessage' : msgObject.success,
     'companyName' : companyName
   });
 });
 
 router.get('/bill-payed', function(req, res, next) {
-    res.render('bill-payed');
-  });
+    res.render('success', {
+      'redirectMessage' : msgObject.billPayed,
+      'companyName' : companyName
+    });
+});
 
 router.get('/error-intention-payment', function(req, res, next) {
-    res.render('error-intention-payment');
+    res.render('success', {
+      'redirectMessage' : msgObject.errorIntentionPayment,
+      'companyName' : companyName
+    });
 });
 
 router.get('/system-disabled', function(req, res, next) {
-    res.render('system-disabled');
+    res.render('success', {
+      'redirectMessage' : msgObject.systemDisabled,
+      'companyName' : companyName
+    });
 });
 
 router.get('/error-bill-draft', function(req, res, next) {
-  res.render('error-bill-draft');
+  res.render('success', {
+    'redirectMessage' : msgObject.errorBillDraft,
+    'companyName' : companyName
+  });
 });
 
 module.exports = router;
