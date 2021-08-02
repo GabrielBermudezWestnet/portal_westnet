8e4f33ed8142026b5df1be4fb7d076fba77c6072
root <root@portal-clientes.westnet.com.ar>
2021-08-02 12:52:01 +0000

    Agrego cambio por solicitud de git

--- routes/portal.js
+++ routes/portal.js
@@ -2,16 +2,17 @@ var express = require('express');
 var router = express.Router();
 companyName = "Westnet S.A";
 msgObject = {
-  success : "Su pago ha sido registrado\nMuchas gracias por usar nuestros servicios de pago",
-  billPayed : "Su factura se encuentra pagada.\nMuchas gracias por usar nuestros servicios de pago",
-  errorIntentionPayment : "Su link de pago no pudo ser generado.\nAguarde unos minutos e intente nuevamente!\nMuchas gracias por usar nuestros servicios de pago",
-  systemDisabled : "El sistema se encuentra inhabilitado, por favor, aguarde unos instantes.\nMuchas gracias por usar nuestros servicios de pago",
-  errorBillDraft : "Esta factura no es valida para ser pagada.\nMuchas gracias por usar nuestros servicios de pago",
+  success : "Su pago ha sido registrado en nuestro sistema.\n Muchas gracias por usar nuestros servicios. ",
+  billPayed : "Su factura ya se encuentra pagada y no registra saldos pendientes.\n Muchas gracias por usar nuestros servicios.",
+  errorIntentionPayment : "Su link de pago no pudo ser generado.\n Intente nuevamente más tarde.\nMuchas gracias por usar nuestros servicios.",
+  systemDisabled : "El sistema se encuentra inhabilitado momentaneamente, por favor, intente nuevamente más tarde. \nMuchas gracias por usar nuestros servicios.",
+  errorBillDraft : "Esta factura no es valida para ser pagada.\nMuchas gracias por usar nuestros servicios.",
+  canceledPay: "Usted ha cancelado su pago. Si desea pagar nuevamente, inicie el proceso nuevamente. \nMuchas gracias por usar nuestros servicios.",
+  notSuccess: "Su pago no pudo ser completado. \n Intente nuevamente más tarde.\nMuchas gracias por usar nuestros servicios.",
 };
 
 /* GET home page. */
 router.get('/payment-intention/:id', function(req, res, next) {
-  console.log(process.env.URL_REDIRECT_GESTION.replace('${customer_id}',req.params.id));
   res.redirect(process.env.URL_REDIRECT_GESTION.replace('${customer_id}',req.params.id));
 });
 
